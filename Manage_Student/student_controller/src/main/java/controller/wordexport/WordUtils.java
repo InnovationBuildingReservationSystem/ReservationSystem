127ce9119cc0b4022ba195d5d0c3a172ee20cf58
[0202zc] <[lzc980202@163.com]>
2019-04-12 23:11:41 +0800

    modify the error

--- Manage_Student/student_controller/src/main/java/controller/wordexport/WordUtils.java
+++ Manage_Student/student_controller/src/main/java/controller/wordexport/WordUtils.java
@@ -40,7 +40,7 @@ public class WordUtils {
             e.printStackTrace();
         }
         /*/usr/local/dataFile/创新楼创客教室学生活动备案审批表.doc*/
-        File outFile = new File("D:/创新楼教室预约系统/outFile/创新楼创客教室学生活动备案审批表.doc"); //导出文件
+        File outFile = new File("/usr/local/dataFile/创新楼创客教室学生活动备案审批表.doc"); //导出文件
         Writer out = null;
         try {
             FileOutputStream fos = new FileOutputStream(outFile);
